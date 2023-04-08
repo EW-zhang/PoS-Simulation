8d0838f06b0a837cb5d81df6a47c7594884825a1
Singularity <52337776+EW-zhang@users.noreply.github.com>
2023-04-08 16:49:38 +0800

    Add files via upload

--- propose_block.py
+++ propose_block.py
@@ -1,24 +1,23 @@
 import requests
+import random
 import time
 
-proposer_index = 0
-random_validators = [8000, 8003, 8004, 8007, 8009, 8010]
-
 while True:
-    proposer = random_validators[proposer_index]
-    committee_ports = random_validators[:proposer_index] + random_validators[proposer_index+1:]
+    # 先只运行区块提议者，不运行验证者委员会
+    random_numbers = random.sample(range(12), 5) #为了模拟真实通信情况
+    random_validators = [num + 8000 for num in random_numbers]
+
+    proposer = random_validators[0]  # 区块提议者
+
+    committee_ports = random_validators[1:]  # 验证者委员会
 
     print(proposer)
     print(committee_ports)
-
     requests.post('http://127.0.0.1:{}/get_ports'.format(proposer), json={"ports": (proposer, committee_ports)})
-
     for committee_port in committee_ports:
-        requests.post('http://127.0.0.1:{}/get_ports'.format(committee_port),
-                      json={"ports": (proposer, committee_ports)})
-
+        requests.post('http://127.0.0.1:{}/get_ports'.format(committee_port), json={"ports": (proposer, committee_ports)})
+    t1 = time.time()
     requests.get('http://127.0.0.1:{}/mine'.format(proposer))
-
+    t2 = time.time()
+    print(t2 - t1)
     time.sleep(1)
-
-    proposer_index = (proposer_index + 1) % len(random_validators)
